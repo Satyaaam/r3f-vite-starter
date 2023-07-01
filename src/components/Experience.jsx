219d7c214c5f0c7fa8c0bf9d295a4ebb223cc5ff
Satyaaam <sattu62025@gmail.com>
2023-07-01 11:22:25 +0530

    initial commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
+import {Environment, OrbitControls, useTexture } from "@react-three/drei";
+import * as THREE from 'three';
 
 export const Experience = () => {
+
+  const map = useTexture(
+    "textures/dreamlike_milky_way_with_blackhole.jpg"
+  )
+
   return (
     <>
+    <ambientLight intensity={0.5}/>
       <OrbitControls />
       <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
+        <sphereGeometry args={[5, 64, 64]} />
+        <meshStandardMaterial map={map} side={THREE.BackSide}/>
       </mesh>
     </>
   );
